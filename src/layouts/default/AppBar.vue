280cf763a9f7aafa5d43a14dde4d1424b62d47a0
2linq <ywyw911@i.softbank.jp>
2023-12-08 14:03:22 +0900

    "conflict"

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -8,7 +8,6 @@
       <v-tab>すべて</v-tab>
       <!--下部scriptタグ内のmethods:にて定義-->
       <v-tab v-on:click="toSpot">スポット</v-tab>
-      <v-tab>交通機関</v-tab>
       <v-tab v-on:click="toEmegency">緊急事態</v-tab>
     </v-tabs>
   </v-card>
